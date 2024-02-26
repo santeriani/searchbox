fa41b84d2d94d13ba577dfadf2054e5a1b56e4b4
Santeri Anias <santeri.anias@gmail.com>
2024-02-26 18:50:01 +0200

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { DATA } from "./Data";
+import Row from "./components/Row";
+import Search from "./components/Search";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    setItems(DATA);
+  })
+
+  const executeSearch = (search) => {
+    const searchArray = DATA.filter((item) => item.lastname.startsWith(search));
+    setItems(searchArray);
+  }
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Search executeSearch={executeSearch} />
+      <FlatList
+        data={items}
+        renderItem = {({item}) => (
+          <Row person={item} />
+        )}
+
+      ></FlatList>
+    </SafeAreaView>
   );
 }
 
---
+++ Data.js
@@ -0,0 +1,62 @@
+export const DATA = [
+    {
+        id: '1',
+        lastname: 'Juntunen'
+    },
+    {
+        id: '2',
+        lastname: 'Ojala'
+    },
+    {
+        id: '3',
+        lastname: 'Korhonen'
+    },
+    {
+        id: '4',
+        lastname: 'Virtanen'
+    },
+    {
+        id: '5',
+        lastname: 'Makinen'
+    },
+    {
+        id: '6',
+        lastname: 'Nieminen'
+    },
+    {
+        id: '7',
+        lastname: 'Laine'
+    },
+    {
+        id: '8',
+        lastname: 'Heikkinen'
+    },
+    {
+        id: '9',
+        lastname: 'Koskinen'
+    },
+    {
+        id: '10',
+        lastname: 'Lehtonen'
+    },
+    {
+        id: '11',
+        lastname: 'Lehtinen'
+    },
+    {
+        id: '12',
+        lastname: 'Saarinen'
+    },
+    {
+        id: '13',
+        lastname: 'Salminen'
+    },
+    {
+        id: '14',
+        lastname: 'Niemi'
+    },
+    {
+        id: '15',
+        lastname: 'Heinonen'
+    },
+];
